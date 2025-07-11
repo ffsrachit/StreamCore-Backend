e01485be02fb96af354b47c371ab3f8064589f10
Rachit Verma <rachitverma2002@gmail.com>
2025-07-12 01:34:36 +0530

    Add healthcheck route and controller

--- src/app.js
+++ src/app.js
@@ -20,9 +20,11 @@ app.use(cookieParser())
 //routes
 
 import userRouter from './routes/user.routes.js'
+import healthcheckRouter from './routes/healthcheck.routes.js'
 
 //routes declaration
-app.use("/api/v1/users" , userRouter)
+app.use("/api/v1/users" , userRouter);
+app.use("/api/v1/healthcheck" , healthcheckRouter);
 
 //https://localhost:8000/api/v1/users/register
 
